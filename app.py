7cfe64c85eb5ecbe4bb9f27063777a21161c7513
Mohammed Laroussi <mnlaroussi@gmail.com>
2018-03-31 23:49:22 +0100

    Added a message in case the word can't be found...

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-	return data[word]
+	if word in data:
+		return data[word]
+	else:
+		return "This word does not exist, please double check!"
 	
 word = input("Enter word: ")
 
