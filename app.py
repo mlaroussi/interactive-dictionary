4ee1534a0b8ed61fe0e5e485d9314e4d6458dc92
Mohammed Laroussi <mnlaroussi@gmail.com>
2018-04-01 14:18:38 +0100

    Added functionalities

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def translate(word):
-	if word in data:
-		return data[word]
+def print_def(word):
+	definition = data[word]
+	if len(definition) > 1 :
+		print ("The word %s has multiple definitions :" % word)
+		for i in definition:
+			print ("* ",i)
+		return ""
 	else:
-		return "This word does not exist, please double check!"
+		print ("* ", data[word][0])
+		return ""
+
+def define(word):
+	if word in data :
+		return print_def(word)
+	else:
+		word = word.lower()
+		if word in data:
+			return print_def(word)
+		elif len(get_close_matches(word, data.keys()) ) > 0 :
+			close_matches = get_close_matches(word,data.keys())
+			yn = input ("Do you mean %s instead ? Y/N :" % close_matches[0]).upper()
+			if yn == "Y":
+				return print_def(close_matches[0])
+			elif yn == "N":
+				return "This word does not exist, sorry...!"
+			else :
+				return "We didn't get your choice, sorry...!"
+		else:
+			return "This word does not exist, please double check!"
 	
 word = input("Enter word: ")
 
-print(translate(word))
+print(define(word))
--- readme.md
+++ readme.md
@@ -4,4 +4,7 @@ This project is my **first** in **python**, following some courses online...
 It is also the **first** one to be worked on **using git** :D
 
 Basically what I have in this project is a JSON file having definitions for words and a python script which loads this latter and try to give a definition to the word in the input...
-
+It currently supports the following:
+- Normal words, with single or multiple definitions
+- Title and Capital case words
+- Correcting the inputed word if it has a typo
